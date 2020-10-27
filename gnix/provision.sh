6f7bdc3d2770336e6d15ba88b8dfe7ddf0c9e3ce
Caromo1 <57388623+Caromo1@users.noreply.github.com>
2020-10-27 13:36:26 +0100

    Copiando archivos sites-enable

--- gnix/provision.sh
+++ gnix/provision.sh
@@ -1,6 +1,6 @@
-sudo apt-get update
-sudo apt-get install nginx
-service nginx status
+sudo apt-get -y update
+sudo apt-get install -y nginx
+service nginx start
 cp -avr /usr/share/nginx/www /vagrant/www
 rm -rf /usr/share/nginx/www
 ln -s /vagrant/www /usr/share/nginx/www
