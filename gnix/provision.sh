7bbe7fcfe01656647c134e85983a86d9aaf376eb
Caromo1 <57388623+Caromo1@users.noreply.github.com>
2020-10-27 13:12:56 +0100

    Realizando comandos en el fichero de provision.sh

--- gnix/provision.sh
+++ gnix/provision.sh
@@ -1,2 +1,6 @@
 sudo apt-get update
 sudo apt-get install nginx
+service nginx status
+cp -avr /usr/share/nginx/www /vagrant/www
+rm -rf /usr/share/nginx/www
+ln -s /vagrant/www /usr/share/nginx/www
