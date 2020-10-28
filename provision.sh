53daa8b919bac29c2bc2bb8b95465c0059da22c8
Caromo1 <57388623+Caromo1@users.noreply.github.com>
2020-10-28 13:35:23 +0100

    Modificando box

---
+++ provision.sh
@@ -0,0 +1,6 @@
+apt-get -y update
+apt-get install -y nginx
+rm -rf /etc/nginx/sites-enabled
+rm -rf "usr/share/nginx/www"
+cp -R /vagrant/sites-enabled /etc/nginx/sites-enabled
+service nginx start
